a3527edc6784500db8266b60acea8cead7c5ca91
jonilsonds9 <jonilsonds9@gmail.com>
2020-05-22 14:41:00 -0300

    aula 2 completa

--- ContaCorrente.js
+++ ContaCorrente.js
@@ -1,51 +1,17 @@
-import { Cliente } from "./Cliente.js";
+import { Conta } from "./Conta.js";
 
-export class ContaCorrente {
+export class ContaCorrente extends Conta {
   static numeroDeContas = 0;
-  agencia;
-  _cliente;
 
-  // #saldo = 0;
-  _saldo = 0;
-  
+  constructor(cliente, agencia) {
+    super(0, cliente, agencia);
 
-  set cliente(novoValor) {
-    if (novoValor instanceof Cliente) {
-      this._cliente = novoValor;
-    }
-  }
-
-  get cliente() {
-    return this._cliente;
-  }
-
-  get saldo() {
-    return this._saldo;
-  }
-
-  constructor(agencia, cliente) {
-    this.agencia = agencia;
-    this.cliente = cliente;
     ContaCorrente.numeroDeContas += 1;
   }
 
+  // sobreescrevendo o comportamento do sacar
   sacar(valor) {
-    if (this._saldo >= valor) {
-      this._saldo -= valor;
-      return valor;
-    }
-  }
-
-  depositar(valor) {
-    if (valor <= 0) {
-      return;
-    }
-
-    this._saldo += valor;
-  }
-
-  transferir(valor, conta) {
-    const valorSacado = this.sacar(valor);
-    conta.depositar(valorSacado);
+    let taxa = 1.1;
+    return this._sacar(valor, taxa);
   }
 }
--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 import { Cliente } from "./Cliente.js";
 import { ContaCorrente } from "./ContaCorrente.js";
+import { ContaPoupanca } from "./ContaPoupanca.js";
 
 const cliente1 = new Cliente("Jonilson", 11122233309);
-const cliente2 = new Cliente("Alice", 88822233309);
 
-const contaCorrenteJonilson = new ContaCorrente(1001, cliente1);
+const contaCorrenteJonilson = new ContaCorrente(cliente1, 1001);
 contaCorrenteJonilson.depositar(500);
-const conta2 = new ContaCorrente(102, cliente2);
+contaCorrenteJonilson.sacar(100);
 
-let valor = 200;
-contaCorrenteJonilson.transferir(valor, conta2);
+const contaPoupanca = new ContaPoupanca(50, cliente1, 1001);
+contaPoupanca.sacar(10);
 
+console.log(contaPoupanca);
 console.log(contaCorrenteJonilson);
-console.log(ContaCorrente.numeroDeContas);
